eba858173c53e0d6eb25b21932d0390ce52fd5a3
songching <songching@c35b54c4-46b1-4f58-8cbd-54a0107d424a>
2004-04-07 15:45:25 +0000

    Forgot to put copyright and GPL license on the file

--- VHDL/Core/MUX3.vhd
+++ VHDL/Core/MUX3.vhd
@@ -1,3 +1,26 @@
+---- $Author: songching $
+---- $Date: 2004-04-07 15:45:25 $
+---- $Revision: 1.2 $
+----------------------------------------------------------------------
+---- $Log: not supported by cvs2svn $
+----------------------------------------------------------------------
+----
+---- Copyright (C) 2004 Song Ching Koh, Free Software Foundation, Inc. and OPENCORES.ORG
+----
+---- This program is free software; you can redistribute it and/or modify
+---- it under the terms of the GNU General Public License as published by
+---- the Free Software Foundation; either version 2 of the License, or
+---- (at your option) any later version.
+----
+---- This program is distributed in the hope that it will be useful,
+---- but WITHOUT ANY WARRANTY; without even the implied warranty of
+---- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+---- GNU General Public License for more details.
+----
+---- You should have received a copy of the GNU General Public License
+---- along with this program; if not, write to the Free Software
+---- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
